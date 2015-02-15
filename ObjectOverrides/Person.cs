32af6d486379b46e3f20eec1db6c25cdf900ec2c
Robert McGIrr <rpm.cpp@live.com>
2015-02-14 22:37:14 -0500

    FIrst cimmitt

---
+++ ObjectOverrides/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Person
+    {
+
+        // FIELD DATA ---------------------------------------------------------------------------
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string SSN { get; set; }
+
+
+
+        // CTORs --------------------------------------------------------------------------------
+
+        public Person(){}
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+
+
+        // METHODS -------------------------------------------------------------------------------
+
+
+        public override string ToString()
+        {
+            string myState = string .Format("[First Name: {0}; Last Name: {1}; Age: {2}]", FirstName, LastName, Age);
+            return myState;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var temp = (Person) obj;
+            if (temp.FirstName == this.FirstName
+                && temp.LastName == this.LastName
+                && temp.Age == this.Age)
+            {
+                return true;
+            }
+            return false;
+
+            // OR
+            //return obj.ToString() == this.ToString();
+            // IF ToString is in good order
+        }
+
+        // Return a hashcode based on unique field (SSN)
+        public override int GetHashCode()
+        {
+            //return SSN.GetHashCode();
+            // Or if nothing unique, hash ToString
+            return this.ToString().GetHashCode();
+        }
+
+
+
+
+    }
+}
+
---
+++ ObjectOverrides/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** FUn with System.Object *****\n");
+
+            Person p1 = new Person("Robert", "McGIrr", 54);
+
+            // Use inherited members of System.Object
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash Code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+
+            // Make some other references to p1
+            Person p2 = p1;
+            object o = p2;
+
+            // Are the references pointing to the same object in memory?
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+            }
+
+            Console.WriteLine(p1.ToString());
+            Console.ReadLine();
+
+            // make identical Persons to test Equals and GetHashCode
+            Person pp1 = new Person("Homer", "Simpson", 50);
+            Person pp2 = new Person("Homer", "Simpson", 50);
+
+            // Get stringed versiuon of objects
+            Console.WriteLine("pp1.ToString() = {0}", pp1.ToString());
+            Console.WriteLine("pp2.ToString() = {0}", pp2.ToString());
+
+            // Test overridden Equals
+            Console.WriteLine("pp1 = pp2?: {0}", pp1.Equals(pp2));
+
+            // Test Hash Codes
+            Console.WriteLine("Same Hash Codes?: {0}", pp1.GetHashCode() == pp2.GetHashCode());
+            Console.WriteLine();
+
+            // Change age of pp2 and test again
+            pp2.Age = 45;
+            Console.WriteLine("pp1.ToString = {0}", pp1.ToString());
+            Console.WriteLine("pp2.ToString = {0}", pp2.ToString());
+            Console.WriteLine("pp1 = pp2?: {0}", pp1.Equals(pp2));
+            Console.WriteLine("Same Hash Codes?: {0}", pp1.GetHashCode() == pp2.GetHashCode());
+            Console.ReadLine();
+
+
+        }
+    }
+}
